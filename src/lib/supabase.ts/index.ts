6ce3cb27150165a0392da3b03540b2dbe8c6d336
pcardenasescobar-ui <pcardenasescobar@gmail.com>
2026-05-21 22:09:21 -0500

    Update index.ts

--- src/lib/supabase.ts/index.ts
+++ src/lib/supabase.ts/index.ts
@@ -1,7 +1,8 @@
-import { createClient as supabaseCreateClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-
-export const createClient = () =>
-  supabaseCreateClient(supabaseUrl, supabaseAnonKey)
+export function createClient() {
+  return createSupabaseClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
